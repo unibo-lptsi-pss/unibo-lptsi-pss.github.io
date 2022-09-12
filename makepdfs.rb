7bc89405e243d4abbb0e7fba78aa7721925ab7b9
Danilo Pianini <danilo.pianini@unibo.it>
2022-09-12 23:51:09 +0200

    use virtual-time-budget=1000000

--- makepdfs.rb
+++ makepdfs.rb
@@ -1,5 +1,5 @@
 #!/usr/bin/env ruby
-command_base = 'google-chrome-stable --headless --run-all-compositor-stages-before-draw --disable-gpu --window-size=1440,900 --virtual-time-budget=10000 --print-to-pdf='
+command_base = 'google-chrome-stable --headless --run-all-compositor-stages-before-draw --disable-gpu --window-size=1440,900 --virtual-time-budget=1000000 --print-to-pdf='
 #slides.pdf ${{steps.gh-pages-url.outputs.URL}}
 command_end = '/#/?print-pdf&pdfSeparateFragments=false'
 root = ARGV[0] || raise("Bad usage, missing argument: launch as ./makepdfs.rb ROOT_OF_THE_WEBSITE")
