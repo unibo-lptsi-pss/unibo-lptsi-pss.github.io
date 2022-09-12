fb77102c60b3af0a725f1a4c76cd11a8b0a58ef8
Danilo Pianini <danilo.pianini@unibo.it>
2022-09-12 18:28:59 +0200

    setup the CI

---
+++ makepdfs.rb
@@ -0,0 +1,24 @@
+#!/usr/bin/env ruby
+command_base = 'google-chrome-stable --headless --run-all-compositor-stages-before-draw --disable-gpu --window-size=1440,900 --virtual-time-budget=10000 --print-to-pdf='
+#slides.pdf ${{steps.gh-pages-url.outputs.URL}}
+command_end = '/?print-pdf&pdfSeparateFragments=false'
+root = ARGV[0] || raise("Bad usage, missing argument: launch as ./makepdfs.rb ROOT_OF_THE_WEBSITE")
+puts "Working inside #{root}"
+remote = `git -C '#{root}' remote get-url origin`
+puts "Detected remote: #{remote}"
+url = remote.match(/^(git@|https:\/\/)github\.com(\/|:)(?<owner>[^\/]+)\/(?<repo>[^\/]+?)(\.git)?$/)
+owner = url[:owner]
+repo = url[:repo] == "#{owner}.github.io" ? "" : "/#{url[:repo]}"
+files = Dir.glob("#{root}/**/index.html")
+puts "Detected the following HTML roots:"
+puts files
+paths = Hash[
+    files.map { |path|
+        route = path.sub(root, '').gsub(/^\/?(.*)\/index.html$/) { $1 }
+        [route.nil? || route.strip.empty? ? "index" : route.gsub('/', '_'), "https://#{owner}.github.io#{repo}/#{route}#{command_end}"]
+    }
+]
+for name, path in paths do
+    puts "Working on #{name} built from #{path}"
+    `#{command_base}#{name}.pdf '#{path}'`
+end
